222dd7e74cd60edf451df4e23a7069e054ecff3f
Kinshuk Sunil <kinshuk@hashstash.in>
2016-03-03 21:29:18 +0530

    prepped app for route serving

--- app/server/publish.js
+++ app/server/publish.js
@@ -1,1 +1,5 @@
-
+Meteor.publish('tasks', function (userId) {
+  // This currently only returns tasks created by the current user
+  // TODO: add a filter mechanic to run custom searches wherever needed
+  return Tasks.find({createdBy: userId});
+});
